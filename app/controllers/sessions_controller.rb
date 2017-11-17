d967d1a2cd29714a6fca1352ea51803d9656baab
Zerina Dragnic <zdragnic@gmail.com>
2017-11-17 21:19:52 +0100

    validacija login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,16 @@ class SessionsController < ApplicationController
       # Log the user in and redirect to the user's show page.
       log_in user
       redirect_to user
+    elsif (params[:session][:username]== '' || params[:session][:password]== '') || (params[:session][:username]== '' && params[:session][:password]== '')
+      # ovaj flash ima jos i :success :info, i sam ce tamo u izgledu promijeniti boje diva u zelenu ili plavu
+      flash.now[:danger] = 'Unesite username i password'
+      render 'new'
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Pogresna username/password kombinacija'
       render 'new'
     end
   end
+
 
   def destroy
     log_out
