209342073fa3bf3601b2b37ff5faba249e4769ba
vildanap <vpanjeta1@etf.unsa.ba>
2017-12-09 01:45:56 +0000

    Obavijest za studente

--- app/helpers/notices_helper.rb
+++ app/helpers/notices_helper.rb
@@ -2,10 +2,13 @@ module NoticesHelper
   def stavioObavijest?(user_id, notice_id)
     korisnik= User.find(user_id)
 
-    var = Notice.where("user_id=? and id=?",user_id, notice_id).exists?
+    #pronaci course_dep_id
+    kurs_id = CourseDepartment.find_by_course_id(notice_id)
+
+    var = UserEnrollment.where("user_id=? and course_department_id=?",user_id, kurs_id).exists?
     puts var
     if korisnik.role_id.equal?(1)
-      puts korisnik.role_id
+     # puts korisnik.role_id
       var = true
     end
     return var
