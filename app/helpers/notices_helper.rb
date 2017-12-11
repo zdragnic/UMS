bc37035881f2cbed3ee6bac704076fbb8e9c1c0b
Zerina Dragnic <zdragnic@gmail.com>
2017-12-11 14:27:02 +0100

    izgled ispravke

--- app/helpers/notices_helper.rb
+++ app/helpers/notices_helper.rb
@@ -6,11 +6,17 @@ module NoticesHelper
     kurs_id = CourseDepartment.find_by_course_id(notice_id)
 
     var = UserEnrollment.where("user_id=? and course_department_id=?",user_id, kurs_id).exists?
+
     puts var
     if korisnik.role_id.equal?(1)
      # puts korisnik.role_id
       var = true
     end
+    if korisnik.role_id.equal?(4)
+      # puts korisnik.role_id
+      var = Notice.where("user_id=? and course_id=?",user_id,notice_id ).exists?
+    end
+
     return var
   end
 end
